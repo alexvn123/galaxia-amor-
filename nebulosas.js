55e6e34c5e80a41444bc1cde00a6819c90fffc02
alexvn123 <alexandervimos12@gmail.com>
2026-08-01 19:11:51 -0500

    Modify nebulosa creation parameters and animation speed

--- nebulosas.js
+++ nebulosas.js
@@ -1,12 +1,12 @@
 let nebulosas=[];
 function crearNebulosas(){
-  const cols=[0xff66ff,0x66ccff,0xaa66ff,0xff99cc];
-  for(let i=0;i<25;i++){
-    const g=new THREE.SphereGeometry(Math.random()*4+3,32,32);
-    const m=new THREE.MeshBasicMaterial({color:cols[Math.floor(Math.random()*cols.length)], transparent:true, opacity:.08});
+  const cols=[0xff66ff,0x66ccff,0xaa66ff,0xff99cc,0xff66aa];
+  for(let i=0;i<30;i++){
+    const g=new THREE.SphereGeometry(Math.random()*5+3,32,32);
+    const m=new THREE.MeshBasicMaterial({color:cols[Math.floor(Math.random()*cols.length)], transparent:true, opacity:.07});
     const n=new THREE.Mesh(g,m);
-    n.position.set((Math.random()-.5)*180,(Math.random()-.5)*60,(Math.random()-.5)*180);
+    n.position.set((Math.random()-.5)*200,(Math.random()-.5)*70,(Math.random()-.5)*200);
     scene.add(n); nebulosas.push(n);
   }
 }
-function animarNebulosas(){nebulosas.forEach(n=>{n.rotation.y+=.0008; n.rotation.x+=.0003;});}
+function animarNebulosas(){nebulosas.forEach(n=>{n.rotation.y+=.0006; n.rotation.x+=.00025;});}
